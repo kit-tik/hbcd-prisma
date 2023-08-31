6c4d208bf398c94099f109c3d0a89fef4cecd1cb
kit-tik <kit-tot@homail.com>
2023-08-31 19:25:20 +0700

    prisma lib commit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,16 @@
 import { PrismaClient } from "@prisma/client";
-declare global {
-    var prisma: PrismaClient;
-}
-if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient();
-}   else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
-}
+// declare global {
+//     var prisma: PrismaClient;
+// }
+// if (process.env.NODE_ENV === "production") {
+//     prisma = new PrismaClient();
+// }   else {
+//     if (!global.prisma) {
+//         global.prisma = new PrismaClient();
+//     }
+//     prisma = global.prisma;
+// }
+
+const prisma = new PrismaClient;
 
 export default prisma;
